98e4f36777cdc7cff1a078164bdad65f802e8372
sboluda <15sergiboluda@gmail.com>
2026-02-25 19:50:29 +0100

    Finished P1

--- P1/compare_sequences/ini_move_seq2.py
+++ P1/compare_sequences/ini_move_seq2.py
@@ -1,4 +1,4 @@
-from score_seqs import score_seqs
+from ini_score_seqs import score_seqs
 
 
 def move_seq2(seq1, seq2):
@@ -10,7 +10,19 @@ def move_seq2(seq1, seq2):
     >>> move_seq2("THEFASTCAT", "THECAT")
     ['THEFASTCAT------', 'THECAT----------', '-THECAT---------', '--THECAT--------', '---THECAT-------', '----THECAT------', '-----THECAT-----', '------THECAT----', '-------THECAT---', '--------THECAT--', '---------THECAT-', '----------THECAT']
     """
-    # YOUR CODE HERE
+    assert len(seq1) != len(seq2)
+    
+    if len(seq1) > len(seq2):
+        largest_alignment, moving_seq = seq1, seq2
+    else:
+        largest_alignment, moving_seq = seq2, seq1
+    
+    num_dashes = len(largest_alignment)
+    full_seq = largest_alignment + "-"*len(moving_seq)
+    movements = [full_seq] + ["-"*i + moving_seq + "-"*(num_dashes-i)
+                              for i in range(len(full_seq) - len(moving_seq) + 1)]
+    
+    return movements
 
 
 def print_scores(seq1, seq2, match, mismatch, gap):
@@ -30,4 +42,12 @@ def print_scores(seq1, seq2, match, mismatch, gap):
     ---------AFASTCAT- -33
     ----------AFASTCAT -36
     """
-    # YOUR CODE HERE
+    list_combinations = move_seq2(seq1, seq2)
+    main_align = list_combinations[0]
+    print(main_align)
+
+    for alignment in list_combinations[1:]:
+        score = score_seqs(main_align, alignment, match, mismatch, gap)
+        print(alignment + " " + str(score))
+        
+    return None
